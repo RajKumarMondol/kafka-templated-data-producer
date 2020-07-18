38dd7a2a48530066a574c504ab6968fd1521145e
Raj Kumar Mondol <rajkumar.mondol@tesco.com>
2020-07-19 00:56:31 +0530

    Update application settings based on command line parameters

--- src/test/java/org/rkm/ktdp/CommandLineApplicationTest.java
+++ src/test/java/org/rkm/ktdp/CommandLineApplicationTest.java
@@ -4,15 +4,20 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class CommandLineApplicationTest {
+    private ApplicationSettings mockApplicationSettings = Mockito.mock(ApplicationSettings.class);
+    private Logger mockLogger = Mockito.mock(Logger.class);
     private CommandLineParams mockCommandLineParams = Mockito.mock(CommandLineParams.class);
 
-    private CommandLineApplication commandLineApplication = new CommandLineApplication(mockCommandLineParams);
+    private CommandLineApplication commandLineApplication = new CommandLineApplication(mockApplicationSettings,
+            mockLogger,
+            mockCommandLineParams);
 
     @Test
     public void shouldDisplayHelpForCommandlineApplication() {
@@ -28,5 +33,23 @@ public class CommandLineApplicationTest {
 
         verify(mockCommandLineParams)
                 .printHelp(any(HelpFormatter.class));
+    }
+
+    @Test
+    public void shouldRunCommandlineApplication() {
+        String[] args = new String[]{"--message-count", "10"};
+        CommandLine mockedCommandLine = Mockito.mock(CommandLine.class);
+
+        when(mockCommandLineParams.purse(args))
+                .thenReturn(mockedCommandLine);
+        when(mockApplicationSettings.toString())
+                .thenReturn("mockSettings.toString()");
+
+        commandLineApplication.run(args);
+
+        verify(mockApplicationSettings)
+                .updateFrom(mockedCommandLine);
+        verify(mockLogger)
+                .info("mockSettings.toString()");
     }
 }
